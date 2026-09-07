80dd4edf7abe2f961e7be8cc659324cf08a95128
Sayed Ali Sena <131773898+Alisenaqubadi@users.noreply.github.com>
2026-09-08 00:31:27 +0500

    feat: Loading and 404 Pages were added

---
+++ trade/src/api/resources.api.js
@@ -0,0 +1,21 @@
+const API_URL = "/api/v1/trade";
+
+export async function getList() {
+  const response = await fetch(`${API_URL}/structure.json`);
+
+  if (!response.ok) {
+    throw new Error("Could not load trade list");
+  }
+
+  return response.json();
+}
+
+export async function getData(id) {
+  const response = await fetch(`${API_URL}/csv/${id}.csv`);
+
+  if (!response.ok) {
+    throw new Error("Could not load trade data");
+  }
+
+  return response.text();
+}
--- trade/src/layouts/Body.jsx
+++ trade/src/layouts/Body.jsx
@@ -1,5 +1,21 @@
-import { Text } from "@mantine/core";
+import { useState } from "react";
+import { useGetData, useGetList } from "../hooks/useResources.query.js";
+import LoadingPage from "../Pages/Loading.jsx";
 
 export default function Body() {
-  return <></>;
+  const [resource , setResource] = useState("210")
+  const { data: list, isLoading: isLoading1 } = useGetList()
+  const { data: MainData, isLoading: isLoading2 } = useGetData(resource)
+
+  if(isLoading1 || isLoading2) {
+    return <LoadingPage />
+    
+  }
+
+
+
+
+  return <>
+    
+  </>;
 }
